8224dc19bff1f7c0f788193565a033513c7bbc31
JavierGavina <gavina@uji.es>
2024-02-05 12:44:29 +0100

    redirection to labelmap.html

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const routines_definition = document.querySelector('#definition-box');
+const routines_simulation = document.querySelector('#simulation-box');
 routines_definition.addEventListener('click', function() {
     window.location.href = "./pages/routines.html"
+})
+routines_simulation.addEventListener('click', function() {
+    window.location.href = "./pages/labelmap.html"
 })
