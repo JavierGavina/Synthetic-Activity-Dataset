c5e3874d6f5b2ee615d26c8f1758065b6e2c962f
JavierGavina <gavina@uji.es>
2024-02-05 12:41:38 +0100

    navegation tools update

---
+++ script.js
@@ -0,0 +1,4 @@
+const routines_definition = document.querySelector('#definition-box');
+routines_definition.addEventListener('click', function() {
+    window.location.href = "./pages/routines.html"
+})
