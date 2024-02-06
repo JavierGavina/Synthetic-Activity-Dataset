7c15fe556b0b402668d60a91e59e2ec32ad432ca
JavierGavina <gavina@uji.es>
2024-02-06 12:38:11 +0100

    completed html for routines simulation

--- pages/utils/labelmap-utils/script.js
+++ pages/utils/labelmap-utils/script.js
@@ -1,4 +1,20 @@
 /*GLOBALIZAR VARIABLE JSON*/
+const dictionary = {
+    "room": 1,
+    "living-room":2,
+    "tv-room":3,
+    "dining-room":4,
+    "garden":5,
+    "terrace":6,
+    "therapy":7,
+    "gym":8,
+    "corridors":9,
+    "bathroom":10,
+    "bedroom":11,
+    "garage":12,
+    "pool":13,
+}
+
 
 var DataFrame = dfjs.DataFrame;
 
@@ -8,8 +24,8 @@ var labelMap = new DataFrame({}, ["Year", "Month", "Day", "Sequence"])
 visualization_labelmap_section = document.querySelector('#visualization-labelmap');
 labelmap_section = document.querySelector('#labelmap');
 
-visualization_labelmap_section.style.display = 'none';
-labelmap_section.style.display = 'none';
+// visualization_labelmap_section.style.display = 'none';
+// labelmap_section.style.display = 'none';
 
 /*ANIMACIONES ZONA DRAGABLE*/
 dragArea = document.querySelector('#drag-area');
@@ -43,13 +59,24 @@ dragArea.addEventListener('click', () => {
     });
 });
 
-
+const downloadDictionaryRooms = (dictionary) => {
+    const dictionary_export = JSON.stringify(dictionary);
+    const blob = new Blob([dictionary_export], {type: "application/json"});
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "dictionary_export.json";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
 
 /*FUNCION PARA PROCESAR EL ARCHIVO JSON*/
 const processFile = (file) => {
     // Comprobar que el archivo es un json
     if (file.type != 'application/json') {
-        alert('El archivo debe ser un JSON');
+        alert('The file must be a JSON');
         return;
     }
     // Leer el archivo como texto
@@ -62,48 +89,65 @@ const processFile = (file) => {
             visualization_labelmap_section.style.display = 'block';
             labelmap_section.style.display = 'block';
             labelMap = getLabelmap(json);
+            if (window.confirm("Do you want to download the dictionary of rooms?")){
+                downloadDictionaryRooms(dictionary);
+            }
 
         } catch (error) {
             alert(error)
-            alert('El archivo no es un JSON válido');
+            alert('The JSON file is not valid');
         }
     };
     reader.onerror = () => {
-        alert('Error al leer el archivo');
+        alert('Error while reading the file');
     };
 }
 
-const convertToMinutes = function(hour){
-    return hour.split(':')[0]*60 + hour.split(':')[1];
+const convertToMinutes = function(hour){ 
+    return parseInt(hour.split(':')[0])*60 + parseInt(hour.split(':')[1]);
 }
 
-const getLabelmap = (json) => {
+const getLabelmap = (json, threshold=0) => {
     dates_labelmap = Object.keys(json);
     dates_labelmap.forEach((date) => {
         let sequence = [];
         let [year, month, day] = date.split('-');
-        
-        init = convertToMinutes(json[date].start_time);
-        end = convertToMinutes(json[date].end_time);
-        room = json[date].room;
-        for (let i = init; i < end; i++){
-            sequence.push(room);
-        }
-
-        labelMap = new DataFrame({
-            "Year": year,
-            "Month": month,
-            "Day": day,
-            "Sequence": sequence
-        });
+        json[date].intervals.forEach((interval, index) => {
+            init = convertToMinutes(interval[0]);
+            end = convertToMinutes(interval[1]);
+            room = json[date].rooms[index];
+            for (let i = init; i < end; i++){
+                sequence.push(room);
+            }
+        })
+        labelMap = labelMap.push({Year: parseInt(year), Month: parseInt(month), Day: parseInt(day), Sequence: sequence})
     })
+}
 
-    window.alert(labelMap.show(5))
-
+const downloadCSV = (data) => {
+    columnas = data.listColumns()
+    filas = data.toArray()
+    const csvContent = "data:text/csv;charset=utf-8," + columnas.join(",") + "\n" + filas.map(row => row.join(",")).join("\n");
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "labelmap.csv");
+    document.body.appendChild(link);
+    link.click();
 }
 
 
+/*CÓDIGO PARA LA SECCIÓN DE VISUALIZACIÓN*/
 
+// THRESHOLD
+
+threshold_input = document.querySelector('#threshold');
+threshold_output = document.querySelector('#threshold-value');
+
+threshold_input.addEventListener("change", () => {
+    threshold_output.textContent = parseInt(threshold_input.value);
+
+}
 
 
 
