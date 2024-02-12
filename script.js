f998615cf63684319ecb36f95f186ce74f6fdd02
JavierGavina <gavina@uji.es>
2024-02-12 10:21:33 +0100

    user-guide intro page updated

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function onPlayerReady(event) {
 
 const routines_definition = document.querySelector('#definition-box');
 const routines_simulation = document.querySelector('#simulation-box');
+const user_guide = document.querySelector("#user-guide-box")
 
 routines_definition.addEventListener('click', function() {
     window.location.href = "./pages/routines.html";
@@ -36,3 +37,7 @@ routines_definition.addEventListener('click', function() {
 routines_simulation.addEventListener('click', function() {
     window.location.href = "./pages/labelmap.html";
 });
+
+user_guide.addEventListener("click", function(){
+    window.location.href = "./pages/user-guide.html"
+})
